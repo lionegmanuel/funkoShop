049c9c4207b09df8b19e8601854559aa20a99f76
MagnuZvn <lionegmanuel@gmail.com>
2023-06-02 21:37:38 -0300

    practica node

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const fs = require('fs');
-fs.readFile('inde.html', 'utf-8', (err, content) => {
-  if (err) throw Error('¡Ocurrió un error inesperado');
-  else console.log(content);
+const EventEmitter = require('events');
 
-});
+const emisorCelulares = new EventEmitter();
+
+emisorCelulares.on('compra', (usuario, localidadDeOrigen) => {
+  console.log(`Compra realizada.Detalles de la compra: \n1 - Nombre: ${usuario}\n2-Origen de la compra: ${localidadDeOrigen}`);
+})
+console.log("Venta de producto N°1:\n")
+emisorCelulares.emit('compra');
+
 
 
