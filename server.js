a1385d2247e58470ad8179c03ce1424b0053fa4b
MagnuZvn <lionegmanuel@gmail.com>
2023-06-05 21:18:11 -0300

    practica nodejs

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
-const EventEmitter = require('events');
 
-const emisorCelulares = new EventEmitter();
+function buyProduct(name) {
+  return new Promise((resolve, reject) => {
+    console.log(`Consultando por disponibilidad de: ${name}...`)
+    setTimeout(() => {
+      if (name === 'Computadora' || name === 'Tablet' || name === 'Celular') {
+        resolve(`Producto adquirido con éxito`);
 
-emisorCelulares.on('compra', (usuario, localidadDeOrigen) => {
-  console.log(`Compra realizada.Detalles de la compra: \n1 - Nombre: ${usuario}\n2-Origen de la compra: ${localidadDeOrigen}`);
-})
-console.log("Venta de producto N°1:\n")
-emisorCelulares.emit('compra');
+      } else reject('Producto NO disponible');
+    }, 3000);
+  });
+}
 
+function productProcess(result) {
+  return new Promise((resolve) => {
+    console.log('Procesando pedido...')
+    setTimeout(() => {
+      console.log(result);
+      resolve('Muchas gracias por tu compra! Te esperamos proximamente...');
+    }, 2000)
+  });
+}
 
+async function pedido(productName) {
+  try {
+    const buy = await buyProduct(productName);
+    console.log(buy);
+    const process = await productProcess(buyProduct);
+    console.log(process);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
+pedido('Celular');
