2ee8a1d0174650196521e45749e899df8f5ff822
MagnuZvn <lionegmanuel@gmail.com>
2023-06-11 20:30:35 -0300

    avance imperceptible

--- server.js
+++ server.js
@@ -1,15 +1,1 @@
-//creacion de servidor
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  console.log('===> Inicializando "REQUEST"....\nDominio solicitado: ' + req.url);
-  console.log("El resultado de la respuesta es... " + res.statusCode)
-  res.end('Respuesta');
-
-});
-
-const port = 3000;
-
-server.listen(port, () => {
-  console.log(`Servidor ejecutado con éxito en http://localhost:${port}...`);
-})
+const url = new URL("https://www.mercadolibre.com.ar");
