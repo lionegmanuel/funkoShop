f0389a9e40995e728538f3740f49f7fb9b0b6e3d
MagnuZvn <manuel.lg077@gmail.com>
2023-06-15 19:59:13 -0300

    instalacion de nodejs en notebook

--- alumnos.js
+++ alumnos.js
@@ -1,23 +1,22 @@
-let listadoAlumnos = [
-  'primer ano' = {
-    'nombre ': 'Juan Carlos Jaramillo',
+let listadoAlumnos = {
+  'primer año': {
+    'nombre': 'Juan Carlos Jaramillo',
     'edad': 18,
     'DNI': 445692435,
-    'Materia Favorita': 'Matematicas'
+    'Materia Favorita': 'Matemáticas'
   },
-  'segundo ano' = {
-    'nombre ': 'Ruben Gonzales',
+  'segundo año': {
+    'nombre': 'Ruben Gonzales',
     'edad': 16,
     'DNI': 45612334,
     'Materia Favorita': 'Lengua y literatura'
   },
-  'tercer ano' = {
-    'nombre ': 'Manuel Lione',
+  'tercer año': {
+    'nombre': 'Manuel Lione',
     'edad': 19,
     'DNI': 44914669,
     'Materia Favorita': 'Ninguna'
   }
+};
 
-]
 module.exports.listadoAlumnos = listadoAlumnos;
-console.log(listadoAlumnos);
--- server.js
+++ server.js
@@ -2,3 +2,47 @@ const http = require("http");
 
 const alumnos = require("./alumnos.js");
 
+const server = http.createServer((req, res) => {
+  //extraccion del metodo de la solicitud mediante desestructuracion de javascript
+  const { method } = req;
+
+  switch (method) {
+    case 'GET':
+      return getControl(req, res);
+    case 'POST':
+      return postControl(req, res);
+    default:
+      console.log(`El servidor NO admite el método ${method}`);
+  }
+})
+const port = 8080;
+server.listen(port, () => {
+  console.log(`SERVIDOR EN PUERTO: ${port}`);
+})
+
+function getControl(req, res) {
+  const path = req.url;
+  //if (path === '/') { req.statusCode = 200; res.end('Ejecución del homepage exitosa') } //cuando se este en la pagina principal
+  switch (path) {
+    case '/':
+      req.statusCode = 200;
+      res.end('Ejecución del homepage exitosa');
+      break;
+    case '/alumnos':
+      req.statusCode = 200;
+      res.end('Listado de alumnos:\n' + JSON.stringify(alumnos.listadoAlumnos));
+      break;
+    case '/alumnos/primer-año':
+      req.statusCode = 200;
+      res.end('Listado de alumnos de 1er año: \n' + JSON.stringify(alumnos.listadoAlumnos["primer año"]));
+      break;
+    case '/alumnos/segundo-año':
+      req.statusCode = 200;
+      res.end('Listado de alumnos de 1er año: \n' + JSON.stringify(alumnos.listadoAlumnos["segundo año"]));
+      break;
+    case '/alumnos/tercer-año':
+      req.statusCode = 200;
+      res.end('Listado de alumnos de 1er año: \n' + JSON.stringify(alumnos.listadoAlumnos["tercer año"]));
+      break;
+  }
+}
