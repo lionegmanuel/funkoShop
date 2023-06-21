97dff245f5ff7ec10b1e84b793dce432f040c7bc
MagnuZvn <lionegmanuel@gmail.com>
2023-06-21 19:47:27 -0300

    avance nodejs uso de express

--- alumnos.js
+++ alumnos.js
@@ -1,22 +1,46 @@
 let listadoAlumnos = {
-  'primer año': {
-    'nombre': 'Juan Carlos Jaramillo',
-    'edad': 18,
-    'DNI': 445692435,
-    'Materia Favorita': 'Matemáticas'
-  },
-  'segundo año': {
-    'nombre': 'Ruben Gonzales',
-    'edad': 16,
-    'DNI': 45612334,
-    'Materia Favorita': 'Lengua y literatura'
-  },
-  'tercer año': {
-    'nombre': 'Manuel Lione',
-    'edad': 19,
-    'DNI': 44914669,
-    'Materia Favorita': 'Ninguna'
-  }
-};
+  primer_ano: [
+    {
+      'id': 1,
+      'nombre': 'Juan Carlos Jaramillo',
+      'edad': 18,
+      'DNI': 445692435,
+      'Materia Favorita': 'Matemáticas'
+    }, {
+      'id': 2,
+      'nombre': 'Manuel Lione',
+      'edad': 19,
+      'DNI': 44914669,
+      'Materia Favorita': 'Lengua'
+    }, {
+      'id': 3,
+      'nombre': 'Juan Fernandez',
+      'edad': 21,
+      'DNI': 42346754,
+      'Materia Favorita': 'Ingenieria y sociedad'
+    }
+  ],
+  segundo_ano: [
+    {
+      'id': 1,
+      'nombre': 'Juan Gabetta',
+      'edad': 45,
+      'DNI': 234456712,
+      'Materia Favorita': 'Carpintería técnica'
+    }, {
+      'id': 2,
+      'nombre': 'Doly Rivero',
+      'edad': 75,
+      'DNI': 156789098,
+      'Materia Favorita': 'Lengua'
+    }, {
+      'id': 3,
+      'nombre': 'Esteban Rodriguez',
+      'edad': 25,
+      'DNI': 39346413,
+      'Materia Favorita': 'Robótica'
+    }
+  ]
+}
 
 module.exports.listadoAlumnos = listadoAlumnos;
--- server.js
+++ server.js
@@ -1,72 +1,35 @@
-const http = require("http");
+const express = require('express'); //importacion de express
+const app = express();
 
-const { listadoAlumnos } = require("./alumnos.js");
+const { listadoAlumnos } = require('./alumnos');
+app.get('/', (req, res) => {
+  res.send('Primer servidor creado con express:\n');
+});
 
-const server = http.createServer((req, res) => {
-  //extraccion del metodo de la solicitud mediante desestructuracion de javascript
-  const { method } = req;
+app.get('/alumnos', (req, res) => {
+  res.send('Primer servidor creado con express:\nListado de alumnos:\n' + JSON.stringify(listadoAlumnos));
+});
+app.get('/alumnos/primer-ano/', (req, res) => {
+  res.send('Alumnos de primer año:\n' + JSON.stringify(listadoAlumnos.primer_ano));
+});
+app.get('/alumnos/primer-ano/:id', (req, res) => {
+  const id = req.params.id;
+  const alumno = listadoAlumnos.primer_ano.filter(alumnoActual => alumnoActual.id === id);
+  if (alumno.length === 0 || alumno == "") res.send('Alumno no encontrado');
+  else res.send('El alumno buscado es: ' + JSON.stringify(alumnoActual));
+});
 
-  switch (method) {
-    case 'GET':
-      return getControl(req, res);
-    case 'POST':
-      return postControl(req, res);
-    default:
-      console.log(`El servidor NO admite el método ${method}`);
-  }
+
+
+
+
+
+
+
+
+
+const port = process.env.port || 8080;
+//escucha del servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
 })
-const port = 8080;
-server.listen(port, () => {
-  console.log(`SERVIDOR EN PUERTO: ${port}`);
-})
-
-function getControl(req, res) {
-  const path = req.url;
-
-  switch (path) {
-    case '/':
-      res.end('Ejecución del homepage exitosa');
-      break;
-    case '/alumnos':
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end('Listado de alumnos:\n' + JSON.stringify(listadoAlumnos));
-      break;
-    case '/alumnos/primer-ano':
-      res.end("Alumnos de primer año:\n" + JSON.stringify(listadoAlumnos["primer año"]));
-      break;
-    case '/alumnos/segundo-ano':
-      res.end('Listado de alumnos de 1er año: \n' + JSON.stringify(listadoAlumnos["segundo año"]));
-      break;
-
-    default:
-      req.statusCode = 400;
-      res.end('¡ERROR!\nParece que la pagina a la que desea ingresar NO existe o está rota.');
-      break;
-  }
-}
-function postControl(req, res) {
-  const path = req.url;
-
-  if (path === '/alumnos') {
-    let body = 'DATOS DEL CUERPO';
-
-    req.on('data', content => {
-      body += content.toString();
-    });
-    req.on('end', () => {
-      console.log(body);
-      console.log(typeof body);
-      //conversion a un objeto javascript para poder manipularlo y acceder a cada una de sus propiedades
-      body = JSON.parse(body);
-      console.log('Despues de convertir a JSON:\n' + body.titulo);
-      res.end('Metodo POST en homepage');
-
-    })
-
-
-
-
-  }
-  if (path === '/alumnos/tercer-ano') res.end('METODO POST aplicado en el listado de alumnos de 1er año: \n' + JSON.stringify(listadoAlumnos["tercer año"]));
-
-}
