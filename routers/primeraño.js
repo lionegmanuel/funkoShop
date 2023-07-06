19e818bded087f4d26ca4adbdc3314f3424a8078
MagnuZvn <lionegmanuel@gmail.com>
2023-07-06 20:34:47 -0300

    finalizacion curso basico nodejs

--- routers/primeraño.js
+++ routers/primeraño.js
@@ -11,12 +11,12 @@ routerPrimerAño.get('/', (req, res) => {
   res.send(JSON.stringify(primer_ano));
 })
 routerPrimerAño.get('/:param', (req, res) => {
-  let parameter = req.params.param;
+  let parametro = req.params.param;
   const regex = /^\d+$/; //patron que permite identificar si el contenido es UNICAMENTE numerico
-  if (regex.test(parameter)) { //ID
-    parameter = parseInt(parameter);
+  if (regex.test(parametro)) { //ID
+    parametro = parseInt(parametro);
     let alumnoActual = "";
-    const resultadoAlumno = primer_ano.filter(alumno => alumno.id === parameter);
+    const resultadoAlumno = primer_ano.filter(alumno => alumno.id === parametro);
     //registro del alumno
     resultadoAlumno.forEach(alumno => {
       alumnoActual = alumno.nombre;
@@ -25,31 +25,69 @@ routerPrimerAño.get('/:param', (req, res) => {
     else res.send(`El alumno encontrado es ${alumnoActual}`);
   }
   else { //MATERIA
-    const resultadoAlumno = primer_ano.filter(alumnoMateria => alumnoMateria.Materia_Favorita.toUpperCase() === parameter.toUpperCase());
-    if (resultadoAlumno.length === 0) res.status(404).send('Ningun alumno tiene la materia ' + parameter.toLocaleLowerCase() + ' asignada como favorita');
+    const resultadoAlumno = primer_ano.filter(alumnoMateria => alumnoMateria.Materia_Favorita.toUpperCase() === parametro.toUpperCase());
+    if (resultadoAlumno.length === 0) res.status(404).send('Ningun alumno tiene la materia ' + parametro.toLocaleLowerCase() + ' asignada como favorita');
     else {
       let alumnoActual = "";
       if (resultadoAlumno.length == 1) {
         resultadoAlumno.forEach(alumno => {
           alumnoActual = alumno.nombre;
         })
-        res.send(`El alumno que tiene la materia ${parameter.toLocaleLowerCase()} como favorita es: ${alumnoActual}`);
+        res.send(`El alumno que tiene la materia ${parametro.toLocaleLowerCase()} como favorita es: ${alumnoActual}`);
       }
       else {
 
         resultadoAlumno.forEach(alumno => {
           alumnoActual += "\n-" + alumno.nombre;
         })
-        res.send('Los alumnos que tienen a la materia ' + parameter.toLocaleLowerCase() + ' como favorita son:\n' + alumnoActual);
+        res.send('Los alumnos que tienen a la materia ' + parametro.toLocaleLowerCase() + ' como favorita son:\n' + alumnoActual);
       }
     }
   }
 })
 routerPrimerAño.post('/', (req, res) => {
-  console.log('Solicitud de tipo "POST" recibida.');
-  let alumnoNuevo = req.body; //extrae el cuerpo de la solicitud
-  listadoAlumnos.push(alumnoNuevo);
-  res.send(JSON.stringify(listadoAlumnos));
+  console.log('Solicitud de tipo "POST" recibida...');
+  let alumnoNuevo = req.body;
+  primer_ano.push(alumnoNuevo);
+  res.send(primer_ano); //envia el listado de alumnos actualizado.
+
+})
+routerPrimerAño.put('/:parametro', (req, res) => {
+  const alumnoActualizado = req.body;
+  let parametro = req.params.parametro;
+  const regex = /^\d+$/;
+  let indice = 0;
+  if (regex.test(parametro)) { //ID
+    id = parseInt(parametro);
+    indice = primer_ano.findIndex(alumno => alumno.id === id);
+  } else indice = primer_ano.findIndex(alumno => alumno.nombre == parametro); //NOMBRE 
+
+  if (indice >= 0) {
+    primer_ano[indice] = alumnoActualizado;
+  }
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(primer_ano));
+})
+routerPrimerAño.patch('/:parametro', (req, res) => {
+  let parametro = req.params.parametro;
+  const regex = /^\d+$/;
+  let indice = 0;
+  const alumnoActualizado = req.body;
+  if (regex.test(parametro)) {
+    id = parseInt(parametro);
+    indice = primer_ano.findIndex(alumno => alumno.id === id)
+  } else indice = primer_ano.findIndex(alumno => alumno.nombre == parametro)
+  if (indice >= 0) {
+    const alumno = primer_ano[indice];
+    Object.assign(alumno, alumnoActualizado);
+  }
+  res.send(JSON.stringify(primer_ano));
+})
+routerPrimerAño.delete('/:id', (req, res) => {
+  const idAlumno = parseInt(req.params.id);
+  const indiceAlumno = primer_ano.findIndex(alumno => alumno.id === idAlumno);
+  if (indiceAlumno >= 0) primer_ano.splice(indiceAlumno, 1);
+  res.send(JSON.stringify(primer_ano));
 })
 module.exports = routerPrimerAño;
 
--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const app = express();
 const { listadoAlumnos } = require('./data/alumnos');
 
 app.get('/', (req, res) => {
-  res.send('Primer servidor creado con express:\n');
+  res.send('<h1>Primer servidor</1>\n<h3>creado con express:</3>');
 });
 const routerAlumnos = require('./routers/alumnos');
 app.use('/alumnos', routerAlumnos);
 const routerPrimerAño = require('./routers/primeraño');
 app.use('/alumnos/primer-ano', routerPrimerAño);
-const routerSegundoAño = require('./routers/segundaoaño');
+const routerSegundoAño = require('./routers/segundoaño');
 app.use('/alumnos/segundo-ano', routerSegundoAño);
 
 
@@ -31,5 +31,5 @@ app.use('/alumnos/segundo-ano', routerSegundoAño);
 const port = process.env.port || 8080;
 //escucha del servidor
 app.listen(port, () => {
-  console.log(`Servidor corriendo en el puerto ${port}`);
+  console.info(`Servidor corriendo en el puerto ${port}`);
 })
